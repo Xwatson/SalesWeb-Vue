99d6205573bbb5c5290ca970be12f8e4eb65c420
Xwatson <973558850@qq.com>
2017-11-15 17:20:00 +0800

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,10 +17,14 @@ module.exports = {
   ],
   // add your custom rules here
   'rules': {
+    "key-spacing": 0,
+    "indent": ["error", 4, {"SwitchCase":1}],
+    "space-before-function-paren" : ["error", "never"],
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 0,
+    "no-eval": 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
