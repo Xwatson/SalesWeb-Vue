3fd29200c98c0ec7ed1cd4b59bf145a48d48d6c8
Xwatson <973558850@qq.com>
2017-11-17 18:13:38 +0800

    ...

---
+++ src/pages/home.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>欢迎</h1>
+</template>
+
+<script>
+export default {
+    name: 'home'
+}
+</script>
+<style lang="scss" scoped>
+    h1{
+        text-align: center
+    }
+</style>
+
+
+
