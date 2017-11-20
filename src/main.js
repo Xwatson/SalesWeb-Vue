e344cd72ef40e170690b7b6e8503f922abcb5fcf
Xwatson <973558850@qq.com>
2017-11-20 17:01:17 +0800

    add elementUI

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import VueI18n from 'vue-i18n'
 import App from './templates/App'
 import store from './store'
 import router from './router'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
 Vue.use(VueI18n)
 
 const i18n = new VueI18n({
@@ -23,6 +26,5 @@ new Vue({
     i18n,
     store,
     router,
-    template: '<App/>',
-    components: { App }
+    render: h => h(App)
 })
